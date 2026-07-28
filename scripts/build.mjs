5bcb20cce58e0525b8d47464e223da8e9b243240
Suraj Rajan <surajkrajan95@gmail.com>
2026-07-28 23:46:56 +0530

    Build with native paths on Windows

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,12 +1,13 @@
 import { spawn } from "node:child_process";
 import { chmod, rm } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
 
 await rm(new URL("../dist", import.meta.url), { recursive: true, force: true });
 
 await new Promise((resolve, reject) => {
   const child = spawn(
     process.execPath,
-    [new URL("../node_modules/typescript/bin/tsc", import.meta.url).pathname],
+    [fileURLToPath(new URL("../node_modules/typescript/bin/tsc", import.meta.url))],
     { stdio: "inherit" },
   );
   child.on("error", reject);
